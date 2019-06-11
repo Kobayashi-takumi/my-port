51ee7b14c14dff857eb720fdf7f76832d2c8379f
Kobayashi-takumi <takumi.kobayashi.development@gmail.com>
2019-06-11 18:10:46 +0900

    create css animetion

--- src/components/HeaderItem.vue
+++ src/components/HeaderItem.vue
@@ -36,4 +36,5 @@ header {
 .el-menu {
     margin: auto;
 }
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,51 @@
 <template>
   <div class="home">
-    <p>Welecom to my portfolio</p>
+    <div class="home-content">
+      <transition> 
+      <h1 class="home-text"><span
+      v-for="(t, index) in text[0]"
+      :key="index"
+      class="item"
+      :style="{animationDelay: index*100+'ms'}"
+      v-text="t"
+      />
+      </h1>
+      </transition>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'home',
+  data() {
+    return {
+      text: ['Welcome to my portfolio', 'Thank you']
+    }
+  }
 }
 </script>
 <style>
+.home-content {
+  width: 100%;
+  height: 500px;
+  display: flex;
+}
+.home-text{
+  height: 100px;
+  margin: auto;
+}
 
+@keyframes text-in {
+  0% {
+    transform: translate(0, -20px);
+    opacity: 0;
+  }
+}
+.item {
+  display: inline-block;
+  min-width: 0.3em;
+  font-size: 2rem;
+  animation: text-in .8s cubic-bezier(0.22, 0.15, 0.25, 1.43) 0s backwards;
+}
 </style>
