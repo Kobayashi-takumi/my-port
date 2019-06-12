73e937ce6e89a44094c43d6a198c333d221d777f
Kobayashi-takumi <takumi.kobayashi.development@gmail.com>
2019-06-12 10:33:53 +0900

    bugfix

--- src/main.js
+++ src/main.js
@@ -11,13 +11,7 @@ Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 var config = {
-  apiKey: "AIzaSyCnJ0r_lJWEV3x34jYcgybYVPbOpA0cV1U",
-  authDomain: "my-portfolio-e30a6.firebaseapp.com",
-  databaseURL: "https://my-portfolio-e30a6.firebaseio.com",
-  projectId: "my-portfolio-e30a6",
-  storageBucket: "my-portfolio-e30a6.appspot.com",
-  messagingSenderId: "68910039940",
-  appId: "1:68910039940:web:222c3191a5578736"
+  //api key を設定
 }
 firebase.initializeApp(config);
 
